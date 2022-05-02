abcc409c60e2936696024f498e35468442fd6343
Sheena Colbert <Coldbear@kias-MacBook-Air.local>
2022-05-02 07:41:00 -0400

    js practice

--- practice.js
+++ practice.js
@@ -3,10 +3,20 @@
 //Copy the value from name to admin.
 //Show the value of admin using alert (must output “John”).
 
-let admin,name; // can declare two variables at once 
-name = 'John';
-admin = name;
-alert(admin);
+//let admin,name; // can declare two variables at once 
+//name = 'John';
+//admin = name;
+//alert(admin);
 
-let ourPlanetName = 'Earth';
-let currentUserName = 'John';
+//let ourPlanetName = 'Earth';
+//let currentUserName = 'John';
+
+function main() {
+    let text1 = "Hello";
+    let text2 = "World";
+    let text3 = text1.concat(" ", text2);
+
+    console.log(text1);
+}
+
+main();
