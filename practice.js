e4dee0f5fa2e0e9049402c4fd99a718444e83758
Sheena Colbert <Coldbear@kias-MacBook-Air.local>
2022-05-02 09:14:23 -0400

    more updates

--- practice.js
+++ practice.js
@@ -13,10 +13,10 @@
 
 function main() {
     let text1 = "Hello";
-    let text2 = "World";
-    let text3 = text1.concat(" ", text2);
+    let text = "HELLO WORLD";
+    let char = text.charAt(0);
 
-    console.log(text1);
+    console.log(text);
 }
 
 main();
